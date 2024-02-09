c6e7efccd138ff293ec2f0ad2577c102cbcd19c1
unknown <smartsam594@gmail.com>
2024-02-09 15:54:45 +0100

    Color Definition

--- lib/main.dart
+++ lib/main.dart
@@ -3,3 +3,20 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const CoffeeApp());
 }
+
+Color myCoffeeColor = const Color(0xFFC77C4D);
+
+class CoffeeApp extends StatelessWidget {
+  const CoffeeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Coffee App",
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const CoffeeHomePage(),
+    );
+  }
+}
